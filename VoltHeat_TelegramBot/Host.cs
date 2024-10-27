f5e11d15c50bf9f4e9353558e8080a8f661b8995
zheni4ka <ebranik15@gmail.com>
2024-10-27 18:24:17 +0200

    zzzzz

--- VoltHeat_TelegramBot/Host.cs
+++ VoltHeat_TelegramBot/Host.cs
@@ -5,15 +5,38 @@ using System.Text;
 using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace VoltHeat_TelegramBot
 {
     public class Host
     {
-        TelegramBotClient _bot;
+        public Action<ITelegramBotClient, Update>? OnMessage;
+        private TelegramBotClient _bot;
+
+        public ReplyKeyboardMarkup replyKeyboard {  get; set; }
+        public ReplyKeyboardMarkup replyKeyboard2 {  get; set; }
+
         public Host(string token)
         {
             _bot = new TelegramBotClient(token);
+
+            replyKeyboard = new ReplyKeyboardMarkup(new[]
+            {
+            new KeyboardButton[] { "\u26A1 Типи електрокотлів", "\U0001F4D9 Контактна інформація"},
+            new KeyboardButton[] { "\U00002139\U0000FE0F  Інформація про бота", "\U0001F916 Чат з АІ" }
+            })
+            {
+                ResizeKeyboard = true
+            };
+
+            replyKeyboard2 = new ReplyKeyboardMarkup(new[]
+            {
+                new KeyboardButton[] { "ТЕНовий котел", "Індукційний котел", "Електродний котел", "\u25C0\uFE0F Назад" }
+            })
+            {
+                ResizeKeyboard = true
+            };
         }
 
         public void Start()
@@ -30,7 +53,8 @@ namespace VoltHeat_TelegramBot
 
         private async Task UpdateHandler(ITelegramBotClient client, Update update, CancellationToken token)
         {
-            Console.WriteLine($"Update handler: {update.Message?.Text ?? "Image"}");
+            Console.WriteLine($"Received message: {update.Message?.Text}");
+            OnMessage?.Invoke(client, update);
             await Task.CompletedTask;
         }
     }
--- VoltHeat_TelegramBot/Program.cs
+++ VoltHeat_TelegramBot/Program.cs
@@ -1,15 +1,111 @@
 ﻿using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
 using VoltHeat_TelegramBot;
+using static System.Net.Mime.MediaTypeNames;
 
 public class Program
 {
     public static string token { get; set; } = "7580066425:AAHM1S9ybfK4t_ZxYc-wky0vtLuJEXWQOQU";
+    public static Host telegramBot = new(token);
+
+    private async Task HandleCallbackQueryAsync(ITelegramBotClient client, CallbackQuery callbackQuery)
+    {
+        //if (callbackQuery.Data == "go_back")
+        //{
+        //    await SelectOption(client, callbackQuery.);
+        //}
+    }
+
+    private static async void SelectOption(ITelegramBotClient client, Update update)
+    {
+        switch (update.Message?.Text)
+        {
+            case "ТЕНовий котел":
+                string tmp = "C:\\Users\\PC\\source\repos\\VoltHeat_TelegramBot\\VoltHeat_TelegramBot\\Images";
+                string text = "ТЕНовий котел опалення є одним із найпоширеніших як у побутовому, так і в промисловому використанні.\r\nПереваги:\r\n1) Простота і надійність: проста конструкція ТЕНових котлів забезпечує їх надійність і зручність в обслуговуванні. Вони ідеально підходять для домовласників, які віддають перевагу простим і зрозумілим технологіям.\r\n2) Економія: ці котли часто дешевші під час купівлі та встановлення, порівняно з іншими видами електрокотлів.\r\nНедоліки:\r\n1) Утворення накипу: в регіонах з жорсткою водою ТЕНи можуть швидко заростати накипом, що вимагає їх регулярного чищення або заміни.\r\n2) Чутливість до напруги: у більшості випадків знадобляться запобіжник від перепадів напруги в мережі.";
+                using (var stream = new FileStream(tmp, FileMode.Open, FileAccess.Read, FileShare.Read))
+                {
+                    await client.SendPhotoAsync(
+                        chatId: update.Message.Chat,
+                        photo: new InputFileStream(stream, Path.GetFileName(tmp)),
+                        caption: text
+                    );
+                }
+                break;
+            case "Індукційний котел":
+                break;
+            case "Електродний котел":
+                break;
+            case "\u25C0\uFE0F Назад":
+                await client.SendTextMessageAsync(
+                chatId: update.Message.Chat,
+                text: "Виберіть пункт",
+                replyMarkup: telegramBot.replyKeyboard
+                );
+                break;
+        }
+    }
+
+
+    private static async void OnMessage(ITelegramBotClient client, Update update)
+    {
+        if (update.Message?.Text == "/menu")
+        {
+            await client.SendTextMessageAsync(
+                chatId: update.Message.Chat,
+                text: "Виберіть пункт",
+                replyMarkup: telegramBot.replyKeyboard
+            );
+        }
+        else
+        {
+            switch (update.Message?.Text)
+            {
+                case "\u26A1 Типи електрокотлів":
+                    await client.SendTextMessageAsync(
+                   chatId: update.Message.Chat,
+                   text: "Ось типи електрокотлів",
+                   replyMarkup: telegramBot.replyKeyboard2
+                   );
+                    break;
+                case "\U0001F4D9 Контактна інформація":
+                    await client.SendTextMessageAsync(
+                   chatId: update.Message.Chat,
+                   text: "option 2"
+                   );
+                    break;
+                case "\U00002139\U0000FE0F Інформація про бота":
+                    await client.SendTextMessageAsync(
+                   chatId: update.Message.Chat,
+                   text: "option 3"
+                   );
+                    break;
+                case "\U0001F916 Чат з АІ":
+                    await client.SendTextMessageAsync(
+                   chatId: update.Message.Chat,
+                   text: "option 4"
+                   );
+                    break;
+                default:
+                    await client.SendTextMessageAsync(
+                   chatId: update.Message.Chat,
+                   text: "no option selected"
+                   );
+                    break;
+            }
+        }
+    }
+
+
+
     public static void Main(string[] args)
     {
-        Host telegramBot = new(token);
         telegramBot.Start();
+        telegramBot.OnMessage += OnMessage;
         Console.ReadLine();
     }
+
+    
 }
