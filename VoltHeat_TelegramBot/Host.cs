36faab29660e776d9f65ef98d48bef0b82d98e56
17:17 <eexet17@gmail.com>
2024-10-27 12:39:12 +0200

    Add project files.

---
+++ VoltHeat_TelegramBot/Host.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace VoltHeat_TelegramBot
+{
+    public class Host
+    {
+        TelegramBotClient _bot;
+        public Host(string token)
+        {
+            _bot = new TelegramBotClient(token);
+        }
+
+        public void Start()
+        {
+            _bot.StartReceiving(UpdateHandler, ErrorHandler);
+            Console.WriteLine("Bot is runnin'");
+        }
+
+        private async Task ErrorHandler(ITelegramBotClient client, Exception exception, CancellationToken token)
+        {
+            Console.WriteLine("Error: " + exception.Message);
+            await Task.CompletedTask;
+        }
+
+        private async Task UpdateHandler(ITelegramBotClient client, Update update, CancellationToken token)
+        {
+            Console.WriteLine($"Update handler: {update.Message?.Text ?? "Image"}");
+            await Task.CompletedTask;
+        }
+    }
+}
---
+++ VoltHeat_TelegramBot/Program.cs
@@ -0,0 +1,15 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using VoltHeat_TelegramBot;
+
+public class Program
+{
+    public static string token { get; set; } = "7580066425:AAHM1S9ybfK4t_ZxYc-wky0vtLuJEXWQOQU";
+    public static void Main(string[] args)
+    {
+        Host telegramBot = new(token);
+        telegramBot.Start();
+        Console.ReadLine();
+    }
+}
